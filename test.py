84f8ced75b6234f78a026193964cbb2a61fc031e
shißdo <61821471+shiueo@users.noreply.github.com>
2023-03-12 00:20:21 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,25 @@
-print("test")
+a = 1
+
+c = []
+for a in range(1, 80):
+    x = 5777 - 2 * a ** 2
+    if x < 0:
+        break
+    c.append(x)
+
+print(c)
+
+print(len(c))
+
+
+def is_prime(n):
+    if n == 1:
+        return False
+    for _ in range(2, int(n ** 0.5) + 1):
+        if n % _ == 0:
+            return False
+    return True
+
+
+for i in c:
+    print(is_prime(i), i)
