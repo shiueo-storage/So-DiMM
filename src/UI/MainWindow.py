e07fab1a8b58ac964c103be608e5a4e53a851b7b
shißdo <61821471+shiueo@users.noreply.github.com>
2023-04-15 23:59:16 +0900

    landmark

--- src/UI/MainWindow.py
+++ src/UI/MainWindow.py
@@ -18,10 +18,12 @@ from PySide6.QtWidgets import (
     QMainWindow,
     QVBoxLayout,
 )
+import mediapipe as mp
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_pose = mp.solutions.pose
 
 global ret, cv_img, CAM_WIDTH, CAM_HEIGHT
-ret = None
-cv_img = None
 
 
 class VIDEOTHREAD(QThread):
@@ -30,10 +32,39 @@ class VIDEOTHREAD(QThread):
     def run(self):
         global ret, cv_img
         cap = cv2.VideoCapture(0)
-        while 1:
-            ret, cv_img = cap.read()
-            if ret:
-                self.change_pixmap_signal.emit(cv_img)
+        with mp_pose.Pose(
+                min_detection_confidence=0.5,
+                min_tracking_confidence=0.5) as pose:
+            while cap.isOpened():
+                success, image = cap.read()
+                if not success:
+                    print("Ignoring empty camera frame.")
+                    # If loading a video, use 'break' instead of 'continue'.
+                    continue
+                else:
+                    self.change_pixmap_signal.emit(image)
+
+                image.flags.writeable = False
+                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                results = pose.process(image)
+                LANDMARK = results.pose_landmarks
+                print(LANDMARK)
+
+                # Draw the pose annotation on the image.
+                """
+                image.flags.writeable = True
+                image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+                mp_drawing.draw_landmarks(
+                    image,
+                    results.pose_landmarks,
+                    mp_pose.POSE_CONNECTIONS,
+                    landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
+                # Flip the image horizontally for a selfie-view display.
+                cv2.imshow('MediaPipe Pose', cv2.flip(image, 1))
+                if cv2.waitKey(5) & 0xFF == 27:
+                    break
+                """
+        cap.release()
 
 
 class sodimm_UI_MainWindow(QMainWindow):
@@ -97,7 +128,7 @@ class sodimm_UI_MainWindow(QMainWindow):
 
     def updateUI(self):
         global CAM_WIDTH, CAM_HEIGHT
-        CAM_WIDTH = self.widget.width()
+        CAM_WIDTH = self.widget.width() // 2
         CAM_HEIGHT = self.widget.height()
 
     @Slot(np.ndarray)
